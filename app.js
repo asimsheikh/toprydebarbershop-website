d28b95303df9e414a866a81e5b9cc23a0bb5abec
asheikh <asim.sardar.sheikh@gmail.com>
2021-10-15 20:53:17 +0200

    Doing a branch to test new styles and techniques

--- app.js
+++
@@ -1,6 +0,0 @@
-const togglebutton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-togglebutton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-})
